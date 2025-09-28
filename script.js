2caa2ccaf717195121d75d9e46353cef0722522b
nolanp75 <nolanpruett@yahoo.com>
2025-09-27 22:07:43 -0400

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+const now = new Date();
+const hours = now.getHours();
+let greeting = '';
+if (hours < 12) {
+    greeting = "Good Morning!";
+} else if (hours < 18) {
+    greeting = "Good Afternoon!";
+} else {
+    greeting = "Good Evening!";
+}   
+
+document.getElementById('greeting').textContent = greeting;
